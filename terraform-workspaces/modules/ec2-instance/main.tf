1471306787b2643e4bd70b7a5fc7e2d1229cde88
sandhyavisakha <sandhyagriddaluru92@gmail.com>
2024-02-10 12:40:35 +0530

    changed main.tf file

--- terraform-workspaces/modules/ec2-instance/main.tf
+++ terraform-workspaces/modules/ec2-instance/main.tf
@@ -1,3 +1,16 @@
 provider "aws" {
     region = "us-east-1"
 }
+
+variable "ami" {
+    description = "This is AMI ID for the instance "
+}
+
+variable "instance_type" {
+    description = "This is the instance type, for example: t2.micro"
+}
+
+resource "aws_instance" "workspaces" {
+    ami = var.ami
+    instance_type = var.instance_type
+}
