7922d262ef2fe3459e3efe68b0b02434f9e99055
sandhyavisakha <sandhyagriddaluru92@gmail.com>
2024-02-10 15:10:46 +0530

    modified for workspaces

--- terraform-workspaces/main.tf
+++ terraform-workspaces/main.tf
@@ -8,10 +8,18 @@ variable "ami" {
 
 variable "instance_type" {
     description = "Instance type of the EC2 instance"
+    type = map(string)
+
+    default = {
+      "dev" = "t2.micro"
+      "stage" = "t2.medium"
+      "prod" = "t2.xlarge"
+    }
 }
 
 module "ec2-instance" {
     source = "./modules/ec2-instance" 
     ami = var.ami
-    instance_type = var.instance_type
+    instance_type = var.instance_type[terraform.workspace]
+
 }
--- terraform-workspaces/terraform.tfvars
+++ terraform-workspaces/terraform.tfvars
@@ -1,2 +1,6 @@
 ami = "ami-0c7217cdde317cfec"
-instance_type = "t2.micro"
+instance_type = {
+  "dev" = "t2.micro"
+  "stage" = "t2.medium"
+  "prod" = "t2.xlarge"
+}
